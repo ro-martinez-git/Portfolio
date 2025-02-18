6cbcd26a4dfc04c31528777620e58a490acfdf76
roci martinez <roomartinez988@gmail.com>
2025-02-18 00:04:31 -0300

    Primer commit

---
+++ app.js
@@ -0,0 +1,58 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+// Importar los routers
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var aboutRouter = require('./routes/about');  // Aquí agregas el aboutRouter
+var contactRouter = require('./routes/contact'); 
+var projectRouter = require('./routes/project'); 
+var skillsRouter = require('./routes/skills'); 
+var formRouter = require('./routes/form'); 
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Usar los routers
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/about', aboutRouter);  // Aquí defines la ruta '/about' usando aboutRouter
+app.use('/contact', contactRouter); 
+app.use('/project', projectRouter); 
+app.use('/skills', skillsRouter); 
+app.use('/form', formRouter); 
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Servidor iniciado en: http://localhost:${port}`);
+});
+
+module.exports = app;
---
+++ routes/form.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET about page. */
+router.get('/', function(req, res, next) {  // Aquí está la ruta configurada como '/'
+  res.render('form', { title: 'Formulario' });
+});
+
+module.exports = router;
